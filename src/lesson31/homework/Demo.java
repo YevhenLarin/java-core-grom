32c354767a6a17d60f63682657139999e124cafb
Dodge <larindodge@gmail.com>
2018-05-30 13:50:19 +0300

    lesson31 v.2

--- src/lesson31/homework/Demo.java
+++ src/lesson31/homework/Demo.java
@@ -5,13 +5,11 @@ import java.util.Arrays;
 public class Demo {
     public static void main(String[] args) {
 
-        String string = "     BJS&*BBBBB(    abs a abs j def 124*%$& ABC";
+        String string = "     BJS&*BBBBB( abs ert   abs a abs j def 124*%$& ABC";
 
         System.out.println(string);
 
         System.out.println(Solution.countSymbols(string));
-
-        System.out.println(Arrays.deepToString(Solution.strWords(string)));
 
         System.out.println(Solution.words(string));
     }
--- src/lesson31/homework/Solution.java
+++ src/lesson31/homework/Solution.java
@@ -8,104 +8,62 @@ public class Solution {
 
     public static Map<Character, Integer> countSymbols(String input) {
 
-        Map<Character, Integer> map = new HashMap<>();
+        if (input.isEmpty())
+            return null;
 
-        char[] chArray = input.trim().toCharArray();
+        ArrayList<Character> chList = new ArrayList<>();
 
-//        ArrayList<Character> chArrayList = new ArrayList<>();
-//
-//        for (Character ch : chArray)
-//            if (Character.isLetter(ch))
-//                chArrayList.add(ch);
-//
-//        for (int i = 0; i < chArrayList.size(); i++) {
-//            if (!map.containsKey(chArrayList.get(i))) {
-//                map.put(chArrayList.get(i), 1);
-//            } else {
-//                map.replace(chArrayList.get(i), map.get(chArrayList.get(i)) + 1);
-//            }
-//        }
-//        return map;
-//    }
+        for (int i = 0; i < input.length(); i++)
+            if (Character.isLetter(input.charAt(i)))
+                chList.add(input.charAt(i));
 
-        char ch;
-        for (int i = 0; i < chArray.length; i++) {
-            int index = 0;
-            if (Character.isLetter(chArray[i])) {
-                index++;
-                ch = chArray[i];
-                chArray[i] = 0;
-                for (int j = i + 1; j < chArray.length; j++) {
-                    if (chArray[j] == ch) {
-                        index++;
-                        chArray[j] = 0;
-                    }
-                }
-                map.put(ch, index);
-            }
-        }
-        return map;
+        return  resMap(chList);
     }
 
     public static Map words(String input) {
 
-        Map<String, Integer> map = new HashMap<>();
+        ArrayList<String> strList = checkWords(input);
 
-        String[] words = strWords(input);
+        return resMap(strList);
+    }
 
-        String str;
-        for (int i = 0; i < words.length; i++) {
-            if (words[i] != null) {
-                str = words[i];
-                int index = 1;
-                words[i] = null;
+    //метод для создание результирующей Map
+    private static <T> Map resMap (ArrayList<T> arrayList) {
 
-                for (int j = i + 1; j < words.length; j++) {
-                    if (words[j].equals(str)) {
-                        index++;
-                        words[j] = null;
-                    }
-                }
-                map.put(str, index);
+        Map<T, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (!map.containsKey(arrayList.get(i))) {
+                map.put(arrayList.get(i), 1);
+            } else {
+                map.replace(arrayList.get(i), map.get(arrayList.get(i)) + 1);
             }
         }
         return map;
     }
 
-    //метод для создания массива стрингов из слов входящего стринга
-    public static String[] strWords(String input) {
+    //метод для создания коллекции из слов входящего стринга
+    private static ArrayList<String> checkWords(String input) {
+
         if (input.isEmpty())
             return null;
 
         String[] strings = input.trim().split(" ");
 
-        int count = 0;
-
-        for (int i = 0; i < strings.length; i++) {
-            for (int j = 0; j < strings[i].length(); j++) {
-                if (!Character.isLetter(strings[i].charAt(j)))
-                    break;
-                if (j == strings[i].length() - 1 && strings[i].length() > 2)
-                    count++;
-            }
-        }
-
-        String[] words = new String[count];
-        int index = 0;
+        ArrayList<String> checkWords = new ArrayList<>();
 
         for (int i = 0; i < strings.length; i++) {
             for (int j = 0; j < strings[i].length(); j++) {
                 if (!Character.isLetter(strings[i].charAt(j)))
                     break;
                 if (j == strings[i].length() - 1 && strings[i].length() > 2) {
-                    words[index] = strings[i];
-                    index++;
+                    checkWords.add(strings[i]);
                 }
             }
         }
-        if (index == 0)
+        if (checkWords.size() == 0)
             return null;
-        return words;
+        return checkWords;
     }
 }
 
